af14fcabe783160f6a450f618854f016b3ae956b
ryan-dev <ryan.11533395@aluno.mg.gov.br>
2026-04-17 08:35:01 -0300

    Add files via upload

---
+++ trabalho_pw/questao_5.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>questao_5</title>
+</head>
+<body>
+    <h1>Questao 5</h1>
+    <?php
+$alunos = ["Ana", "Bruno", "Carlos", "Diana", "Eduardo"];
+
+foreach ($alunos as $nome) {
+    echo "Seja bem-vindo, $nome <br>";
+}
+    ?>
+    <br>
+    <a href="questao_4.php">Questao 4</a><br>
+    <a href="questao_6.php">Questao 6</a>
+</body>
+</html>
---
+++ trabalho_pw/questao_7.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>questao_7</title>
+</head>
+<body>
+    <h1>Questao 7</h1>
+    <?php
+$livro = [
+    "titulo" => "O Senhor dos Anéis <br>",
+    "autor" => "J.R.R. Tolkien <br>",
+    "ano" => 1954
+];
+
+echo "--- Detalhes do Livro --- <br>";
+echo "Título: " . $livro["titulo"] . "\n";
+echo "Autor: " . $livro["autor"] . "\n";
+echo "Ano de Publicação: " . $livro["ano"] . "";
+    ?>
+    <br>
+    <a href="questao_6.php">Questao 6</a><br>
+    <a href="questao_8.php">Questao 8</a>
+    
+</body>
+</html>
